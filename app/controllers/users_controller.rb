47d2aa5efbc4e3318cb0ded8fa7f859360033787
Ichigo Hoshimiya <socket65536@gmail.com>
2016-09-25 22:29:33 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # ここで @user は user_url(@user) と同義
     else
       render 'new'
     end
