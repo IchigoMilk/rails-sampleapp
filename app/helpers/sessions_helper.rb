d65e0d1f0632267e7da0d12fb4a10281ab19d6a5
Ichigo Hoshimiya <socket65536@gmail.com>
2016-10-18 23:21:12 +0900

    8.2.3 Layout Links

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+
+  # 渡されたユーザでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 現在ログインしているユーザを返す(ユーザがログイン中のときのみ)
+  def current_user
+    # この ||= はメモ化の目的
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # ユーザがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
 end
