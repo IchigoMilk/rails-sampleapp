f0ae5aee0cbb6dc376203aaeb2563713ec857a7c
Ichigo Hoshimiya <socket65536@gmail.com>
2016-11-01 23:11:14 +0900

    8.4.2 keep login status

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,10 +5,25 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # 現在ログインしているユーザを返す(ユーザがログイン中のときのみ)
+  # ユーザを永続的セッションに記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 記憶トークンcookiesに対応するユーザを返す
   def current_user
-    # この ||= はメモ化の目的
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      # この ||= はメモ化の目的
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # ユーザがログインしていればtrue、その他ならfalseを返す
