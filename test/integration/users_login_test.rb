b7dd96d6c61d4c68d99d7c1adb133de5af8d6aec
Ichigo Hoshimiya <socket65536@gmail.com>
2016-11-15 22:06:51 +0900

    Finish log in/log out exercises 8.6.2

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -40,9 +40,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    # よくわからん
-    # assert_equal FILL_IN, assigns(:user).FILL_IN
-    assert_not_nil cookies['remember_token']
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+    # assert_not_nil cookies['remember_token']
   end
 
   test "login without remembering" do
