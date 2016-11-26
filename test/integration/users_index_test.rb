4ad41037deacbfd926150fb4339a1af179caeaa7
Ichigo Hoshimiya <socket65536@gmail.com>
2016-11-26 23:04:24 +0900

    Finish password reset exercises

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -12,13 +12,30 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_template 'users/index'
     assert_select 'div.pagination'
     User.paginate(page: 1).each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), text: user.name, count: 0
       end
     end
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
+    end
+  end
+
+  test "user profile links" do
+    log_in_as(@admin)
+    get users_path
+    User.paginate(page: 1).each do |user|
+      get user_path(user)
+      if user.activated?
+        assert_template 'users/show'
+      else
+        assert_template layout: nil
+      end
     end
   end
 
