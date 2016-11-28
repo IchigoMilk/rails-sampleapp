78fcce969db05d3f17780d491d745e05b6a2f140
Ichigo Hoshimiya <socket65536@gmail.com>
2016-11-28 23:00:45 +0900

    13.3.2 create microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,13 +57,6 @@ class UsersController < ApplicationController
     end
 
     # *before action* filter
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in first"
-        redirect_to login_url
-      end
-    end
 
     # 正しいユーザかどうか確認
     def correct_user
