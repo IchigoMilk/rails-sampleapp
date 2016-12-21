dc8ae5fc06768e690cacff9dc50aff8428b91a4a
Ichigo Hoshimiya <socket65536@gmail.com>
2016-12-21 23:15:04 +0900

    14.1.3 validation of relationship

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @relationship = Relationship.new(follower_id: users(:honoka).id,
+                                     followed_id: users(:hikari).id)
+  end
+
+  test "should be valid" do
+    assert @relationship.valid?
+  end
+
+  test "should require a follower id" do
+    @relationship.follower_id = nil
+    assert_not @relationship.valid?
+  end
+
+  test "should require a followed id" do
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
+  end
 end
