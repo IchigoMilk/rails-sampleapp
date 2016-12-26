0ce4537a2d8f6c45a6f74a5c1a365ce2dde55c57
Ichigo Hoshimiya <socket65536@gmail.com>
2016-12-27 00:00:30 +0900

    Add test 14.27

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -66,7 +66,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not @other_user.admin?
     patch user_path(@other_user), params: { user: { password: 'password',
                                                     password_confirmation: 'password',
-                                                    admin: true } } 
+                                                    admin: true } }
     assert_not @other_user.reload.admin?
+  end
+
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
   end
 end
